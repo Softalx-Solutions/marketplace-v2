87a43202ccd2fb1c34a2d4c275a316f1ba16fd53
lesley ayemi <leeayemi@gmail.com>
2023-08-28 15:47:49 +0100

    captcha

--- marketplace/forms.py
+++ marketplace/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-# from captcha.fields import CaptchaField
+from captcha.fields import CaptchaField
 
 from marketplace.models import ContactUsModel
 
 class ContactForm(forms.ModelForm):
-    # captcha = CaptchaField()
+    captcha = CaptchaField()
 
     class Meta:
         model = ContactUsModel
