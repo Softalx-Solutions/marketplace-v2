33d7d90df9bca74a072cfff75c12758391d188bf
lesley ayemi <leeayemi@gmail.com>
2023-09-05 05:27:31 +0100

    logo added

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -92,16 +92,16 @@ $(".dark").on('click', function (e) {
 	$(".body").addClass("is__dark")
 	$(".light").removeClass("is_active")
 	$(".dark").addClass("is_active")
-	document.getElementById("logo_js").src = "/static/assets/img/logos/craftsfest.png";
-	document.getElementById("logo_js_f").src = "/static/assets/img/logos/craftsfest.png";
+	document.getElementById("logo_js").src = "/static/assets/img/logos/zenarts-blue.jpeg";
+	document.getElementById("logo_js_f").src = "/static/assets/img/logos/zenarts-blue.jpeg";
 
 });
 $(".light").on('click', function (e) {
 	$(".body").removeClass("is__dark")
 	$(".light").addClass("is_active")
 	$(".dark").removeClass("is_active")
-	document.getElementById("logo_js").src = "/static/assets/img/logos/craftsfest.png";
-	document.getElementById("logo_js_f").src = "/static/assets/img/logos/craftsfest.png";
+	document.getElementById("logo_js").src = "/static/assets/img/logos/zenarts-blue.jpeg";
+	document.getElementById("logo_js_f").src = "/static/assets/img/logos/zenarts-blue.jpeg";
 });
 
 // menu popup
