4b21ab7b1efd99e21b8525e5bfe50c2edd4a4c90
lesley ayemi <leeayemi@gmail.com>
2024-05-13 00:25:29 +0100

    collectfast

--- config/settings.py
+++ config/settings.py
@@ -245,6 +245,7 @@ else:
 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 
 
 
